8f2b3d03e8c180b0266fcf432c366eac52898cf3
zerosnacks <zerosnacks@protonmail.com>
2023-08-17 23:49:28 +0200

    add hooks test

--- test/ERC6909.t.sol
+++ test/ERC6909.t.sol
@@ -5,27 +5,80 @@ import "./utils/SoladyTest.sol";
 
 import {ERC6909, MockERC6909} from "./utils/mocks/MockERC6909.sol";
 
+contract MockERC6909WithHooks is MockERC6909 {
+    uint256 public beforeCounter;
+    uint256 public afterCounter;
+
+    function _useBeforeTokenTransfer() internal view virtual override returns (bool) {
+        return true;
+    }
+
+    function _useAfterTokenTransfer() internal view virtual override returns (bool) {
+        return true;
+    }
+
+    function _beforeTokenTransfer(address, address, uint256, uint256) internal virtual override {
+        beforeCounter++;
+    }
+
+    function _afterTokenTransfer(address, address, uint256, uint256) internal virtual override {
+        afterCounter++;
+    }
+}
+
+contract ERC6909HooksTest is SoladyTest {
+    uint256 public expectedBeforeCounter;
+    uint256 public expectedAfterCounter;
+
+    function _checkCounters() internal view {
+        require(
+            expectedBeforeCounter == MockERC6909WithHooks(msg.sender).beforeCounter(),
+            "Before counter mismatch."
+        );
+        require(
+            expectedAfterCounter == MockERC6909WithHooks(msg.sender).afterCounter(),
+            "After counter mismatch."
+        );
+    }
+
+    function _testHooks(MockERC6909WithHooks token) internal {
+        address from = _randomNonZeroAddress();
+        expectedBeforeCounter++;
+        expectedAfterCounter++;
+        token.mint(address(this), 1, 1000);
+
+        expectedBeforeCounter++;
+        expectedAfterCounter++;
+        token.transferFrom(address(this), from, 1, 1000);
+
+        vm.prank(from);
+        expectedBeforeCounter++;
+        expectedAfterCounter++;
+        token.transferFrom(from, address(this), 1, 1);
+
+        expectedBeforeCounter++;
+        expectedAfterCounter++;
+        token.burn(address(this), 1, 1);
+    }
+
+    function testERC6909Hooks() public {
+        MockERC6909WithHooks token = new MockERC6909WithHooks();
+
+        for (uint256 i; i < 32; ++i) {
+            _testHooks(token);
+        }
+    }
+}
+
 contract ER6909Test is SoladyTest {
     MockERC6909 token;
 
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 indexed id,
-        uint256 amount
-    );
+    event Transfer(address indexed from, address indexed to, uint256 indexed id, uint256 amount);
 
-    event OperatorSet(
-        address indexed owner,
-        address indexed spender,
-        bool approved
-    );
+    event OperatorSet(address indexed owner, address indexed spender, bool approved);
 
     event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 indexed id,
-        uint256 amount
+        address indexed owner, address indexed spender, uint256 indexed id, uint256 amount
     );
 
     struct _TestTemps {
@@ -97,10 +150,7 @@ contract ER6909Test is SoladyTest {
         vm.prank(t.from);
         token.transfer(t.to, t.id, t.transferAmount);
 
-        assertEq(
-            token.balanceOf(t.from, t.id),
-            t.mintAmount - t.transferAmount
-        );
+        assertEq(token.balanceOf(t.from, t.id), t.mintAmount - t.transferAmount);
         assertEq(token.balanceOf(t.to, t.id), t.transferAmount);
         assertEq(token.totalSupply(t.id), t.mintAmount);
     }
@@ -121,10 +171,7 @@ contract ER6909Test is SoladyTest {
         vm.prank(t.to);
         token.transferFrom(t.from, t.to, t.id, t.transferAmount);
 
-        assertEq(
-            token.balanceOf(t.from, t.id),
-            t.mintAmount - t.transferAmount
-        );
+        assertEq(token.balanceOf(t.from, t.id), t.mintAmount - t.transferAmount);
         assertEq(token.balanceOf(t.to, t.id), t.transferAmount);
         assertEq(token.totalSupply(t.id), t.mintAmount);
     }
@@ -139,31 +186,19 @@ contract ER6909Test is SoladyTest {
         assertEq(token.isOperator(t.from, t.to), t.approved);
     }
 
-    function _expectTransferEvent(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount
-    ) internal {
+    function _expectTransferEvent(address from, address to, uint256 id, uint256 amount) internal {
         vm.expectEmit(true, true, true, true);
         emit Transfer(from, to, id, amount);
     }
 
-    function _expectOperatorSetEvent(
-        address owner,
-        address spender,
-        bool approved
-    ) internal {
+    function _expectOperatorSetEvent(address owner, address spender, bool approved) internal {
         vm.expectEmit(true, true, true, true);
         emit OperatorSet(owner, spender, approved);
     }
 
-    function _expectApprovalEvent(
-        address owner,
-        address spender,
-        uint256 id,
-        uint256 amount
-    ) internal {
+    function _expectApprovalEvent(address owner, address spender, uint256 id, uint256 amount)
+        internal
+    {
         vm.expectEmit(true, true, true, true);
         emit Approval(owner, spender, id, amount);
     }
